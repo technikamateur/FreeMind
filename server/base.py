ad352de6bfa4de3dbf59c57dfedb6b022b21a889
Daniel <technikamateur@gmx.de>
2016-12-21 09:51:32 +0100

    changed license to GPLv3 in scripts

--- server/base.py
+++ server/base.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+
+# FreeMind is a composition of software and config files. It will help you to manage your Linux fileserver.
+# Copyright (C) 2017  Daniel Körsten aka TechnikAmateur
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # -*- coding: utf-8 -*-
 """
 This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.

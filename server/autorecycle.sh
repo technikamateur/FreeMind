1014f46cca653b93cce48ef8d0ce363d21897cdc
Daniel <technikamateur@gmx.de>
2017-01-20 15:32:11 +0100

    modified overveiw, added autorecycle script

---
+++ server/autorecycle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# FreeMind is a composition of software and config files. It will help you to manage your Linux fileserver.
+# Copyright (C) 2017  Daniel Körsten aka TechnikAmateur
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# run script as sudo. Maybe a check at the beginnning would be helpful
+sleep 30
+ready="$(python3 /etc/freemind/fmmain.py recycleready get)"
+if ! [[ "$ready" == "1" ]]; then
+  exit 1 # Es sollte vermerkt werden, ob das Script lief oder nicht. Oder ist das sinnlos?
+fi
+rsync -a /media/fileraid/.recyclebin/ /media/recycledrive

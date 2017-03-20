af6b10d15573f1a54a0359dae4d72e8d4ddbdf67
Daniel <technikamateur@gmx.de>
2017-03-20 15:58:00 +0100

    spacegrabber ready

--- server/fmmain.py
+++ server/fmmain.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 """
 FreeMind is a composition of software and config files. It will help you to manage your Linux fileserver.
 Copyright (C) 2017  Daniel Körsten aka TechnikAmateur
@@ -43,31 +42,4 @@ elif sys.argv[1] == "recycleready":
     status = dbase.recycleready(recyclepara)
     if (status == "1") or (status == "0"):
         print(status)
-elif sys.argv[1] == 1:
-    # run function for collecting data
-    hddnames, hddarray, number = libary.spacagrabber()
-    # creating arrays for webinterface
-    hddname = [0] * number
-    hdds = [0] * number
-    memis = [0] * number
-    memtotal = [0] * number
-    # number -1 because loop starts with 0 instead of 1
-    number -= 1
-    # loop for text splitting
-    for i in range(0,number):
-        hddarraysplit = hddarray[i].split("+")
-        hdds[i] = hddarraysplit[0]
-        memis[i] = hddarraysplit[1]
-        memtotal[i] = hddarraysplit[2]
-    for i in range(0,number):
-        hddnamesplit = hddnames[i].split("=")
-        j = 0
-        for elements in hdds:
-            if hdds[j] == hddnamesplit[0]:
-                hddname[j] = hddnamesplit[1]
-                break
-            j += 1
-#    else:
-#        error = XYZ
-#        dbase.inserterror(error)
-# memtotal und memis fehlen noch vollstaendig
+elif sys.argv[1] == 1: # spacegrabber
--- server/libary.py
+++ server/libary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 """
 FreeMind is a composition of software and config files. It will help you to manage your Linux fileserver.
 Copyright (C) 2017  Daniel Körsten aka TechnikAmateur
@@ -19,8 +18,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import time
 import os
+import subprocess
+import time
 
 
 # give this function 2 dates and you will get timediff
@@ -40,16 +40,25 @@ def timediff(olddate, newdate):
     diff = diff // 86400  # ganzzahlige division durch 24h, da Ergebnis in Sekunden
     return diff
 
-def spacagrabber():
+
+def spacegrabber():
     # first, it should start the .sh
-    # check number of drives and create array
+    # what happens if drive is not present?????
+    subprocess.call("./spacegrabber.sh")  # if an error occurs: use(["script"])
+    # check number of drives and create array size for disks
     fobj = open("mem.dat")
-    count = 0
+    countdisks = 0
     for line in fobj:
-        count += 1
+        countdisks += 1
     fobj.close()
-    darray = [0] * count
-    dnames = [0] * count
+    darray = [0] * countdisk
+    # check number of names and create array size for names
+    fobj = open("defhdd.conf")
+    countnames = 0
+    for line in fobj:
+        countnames += 1
+    fobj.close()
+    dnames = [0] * countnames
     # write data from file into array
     fobj = open("mem.dat")
     i = 0
@@ -65,22 +74,63 @@ def spacagrabber():
     for line in fobj:
         dnames[i] = line.rstrip()
         i += 1
-        if i == count:
-            break
     fobj.close()
-    #return dnames, darray, count
     # creating array for every category
-    hddname = [0] * count
-    hdd = [0] * count
-    memis = [0] * count
-    memtotal = [0] * count
-    # count -1 because loop starts with 0 instead of 1
-    count -= 1
-    # loop for text splitting (mem.dat)
-    for disks in darray:
-        hddarraysplit = disks.split("+")
+    hddname = [0] * countdisks
+    hdd = [0] * countdisks
+    memis = [0] * countdisks
+    memtotal = [0] * countdisks
+    mempercent = [0] * countdisks
+    # loop for disk text splitting (mem.dat) i=0
+    for i in range(len(darray)):
+        hddarraysplit = darray[i].split("+")
         hdd[i] = hddarraysplit[0]
         memis[i] = hddarraysplit[1]
         memtotal[i] = hddarraysplit[2]
-    for names in dnames:
-        hddnamesplit = names.split("=")
+    # setting all hdd names to "not given"
+    for i in range(len(hddname)):
+        hdd[i] = "not given"
+    # loop for name text splitting (defhdd.conf) i = 0; try to replace "not given" by a name
+    for i in range(len(dnames)):
+        try:
+            hddnamesplit = dnames[i].split("=")
+            if hdd[i] == hddnamesplit[0]:
+                hddname[i] = hddnamesplit[1]
+        # exception for statements without "=" e.g. comments: pass them!
+        except:
+            pass
+    # processing memis and memtotal
+    # replace strings below
+    for i in range(len(memis)):
+        memis[i] = memis[i].replace(",", ".")
+        memis[i] = memis[i].replace("TiB", "T")
+        memis[i] = memis[i].replace("GiB", "G")
+        memis[i] = memis[i].replace("MiB", "M")
+        memtotal[i] = memtotal[i].replace(",", ".")
+        memtotal[i] = memtotal[i].replace("TiB", "T")
+        memtotal[i] = memtotal[i].replace("GiB", "G")
+        memtotal[i] = memtotal[i].replace("MiB", "M")
+        # convert everything to G
+        if memis[i].endswith("T"):
+            memis[i] = memis[i].replace("T", "")
+            memis[i] = int(memis[i]) * 1000
+            #memis[i] = str(memis[i]) + "G"
+        elif memtotal[i].endswith("T"):
+            memtotal[i] = memtotal[i].replace("T", "")
+            memtotal[i] = int(memtotal[i]) * 1000
+            #memtotal[i] = str(memtotal[i]) + "G"
+        elif memis[i].endswith("G"):
+            memis[i] = memis[i].replace("G", "")
+        elif memtotal[i].endswith("G"):
+            memtotal[i] = memtotal[i].replace("G", "")
+        elif memis[i].endswith("M"):
+            memis[i] = memis[i].replace("M", "")
+            memis[i] = int(memis[i]) / 1000
+            #memis[i] = str(memis[i]) + "G"
+        elif memtotal[i].endswith("M"):
+            memtotal[i] = memtotal[i].replace("M", "")
+            memtotal[i] = int(memtotal[i]) / 1000
+            #memtotal[i] = str(memtotal[i]) + "G"
+        # calculate disk usage in percent
+        mempercent[i] = (100 * memis[i]) / memtotal[i]
+    # return whatever

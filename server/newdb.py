40939fd0871d2df21a5a7c5be576d0609e36c2f1
Daniel <technikamateur@gmx.de>
2017-03-29 09:36:42 +0200

    newdbupdate

--- server/newdb.py
+++ server/newdb.py
@@ -19,8 +19,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import sqlite3
 import os
+import sqlite3
 
 
 # function create databse if not exists
@@ -33,12 +33,12 @@ def create():
                           id INTEGER PRIMARY KEY,
                           error INTEGER,
                           date TEXT,
-                          time TEXT);""")
+                          time TEXT);""") # kann wech
         cursor.execute("""CREATE TABLE IF NOT EXISTS updatelog(
                           id INTEGER PRIMARY KEY,
                           date TEXT,
                           time TEXT,
-                          updatetyp INTEGER);""")
+                          updatetyp INTEGER);""") # kann wech
         cursor.execute("""CREATE TABLE IF NOT EXISTS recycleready(
                           id INTEGER PRIMARY KEY,
                           ready INTEGER);""") # 0 = not ready, 1 = ready
@@ -54,18 +54,20 @@ def create():
                           id INTEGER PRIMARY KEY,
                           date TEXT,
                           time TEXT,
-                          updatetyp INTEGER);""")
+                          typ INTEGER);""")
         connection.close()
+
 
 # function insert data in database
 # v 1.0 - NOT final
-def insert(dbtarget, dbdataarray): # dbtarget: table, dbdataarray: data for table
+def insert(dbtarget, dbdata): # dbtarget: table, dbdataarray: data for table
+
     if dbtarget == 1: # insert backupready
         connection = sqlite3.connect("freemind.db")
         cursor = connection.cursor()
         try:
             # yapf: disable
-            cursor.execute("""UPDATE backupready SET ready = ?
+            cursor.execute("""UPDATE backupready SET ready=?
                               WHERE id = 1""", (ready))
             connection.commit()
             connection.close()
@@ -82,6 +84,19 @@ def insert(dbtarget, dbdataarray): # dbtarget: table, dbdataarray: data for tabl
         cursor = connection.cursor()
         try:
             # yapf: disable
+            cursor.execute("""UPDATE recycleready SET ready=?
+                              WHERE id=1""", (ready))
+            connection.commit()
+            connection.close()
+            # yapf: enable
+        except:
+            # yapf: disable
+            cursor.execute("""INSERT INTO recycleready(id, redeay)
+                              VALUES(1,?)""", (ready))
+            connection.commit()
+            connection.close()
+            # yapf: enable
+    #else...
 
 
 # function get data from database
@@ -89,14 +104,30 @@ def insert(dbtarget, dbdataarray): # dbtarget: table, dbdataarray: data for tabl
 def get(dbtarget):
     if dbtarget == 1: # get backupready
         try:
-            # yapf: disable
             connection = sqlite3.connect("freemind.db")
-            cursor = connection.cursor()#
-            cursor.execute("""SELECT * FROM backupready WHERE id=1""")
+            cursor = connection.cursor()
+            # yapf: disable
+            cursor.execute("""SELECT * FROM backupready WHERE id=1""") # * should be replaced by colum
+            # yapf: enable
             ready = cursor[1]
             connection.close()
-            # yapf: enable
         except:
-            ready = "error"
+            ready = "err"
         ready = str(ready)
+        return ready
+    elif dbtarget == 2: # get recycleready
+        try:
+            connection = sqlite3.connect("freemind.db")
+            cursor = connection.cursor()
+            # yapf: disable
+            cursor.execute("""SELECT * FROM recycleready WHERE id=1""")
+            # yapf: enable
+            ready = cursor[1]
+            connection.close()
+        except:
+            ready = "err"
+        ready = str(ready)
+        return ready
+    else:
+        ready = "value"
         return ready

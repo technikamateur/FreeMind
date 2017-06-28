411e4d5fc7bb14ce98bc90b72c256f984553f796
Daniel <technikamateur@gmx.de>
2017-06-28 15:17:51 +0200

    huge cleanup

--- testdir/reader.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-t = 1
-t = int(t)
-connection = sqlite3.connect("test.db")
-cursor = connection.cursor()
-cursor.execute("""SELECT * FROM memory WHERE drive=1""")
-print(cursor.fetchone())
--- testdir/testit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ $EUID != 0 ]]; then
-  echo "Please run script as root!"
-  exit 1
-else
-  echo "Wie erwartet, ich bin nicht root..."
-  sudo sh -c 'echo $SUDO_USER'
-  exit 0
-fi
